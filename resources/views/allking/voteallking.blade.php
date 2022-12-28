5bbe89455ba704eeaaab63da5d241c4500003e9b
aunglynn550 <aunglynn550@gmail.com>
2022-12-28 23:56:32 +0630

    format

--- resources/views/allking/voteallking.blade.php
+++ resources/views/allking/voteallking.blade.php
@@ -6,8 +6,9 @@
     <div class="voting-container">
     <form action="{{ route('voteallking') }}" method="post">
     @csrf
-    <label for="roll_no">Roll No</label>
+    <label for="roll_no" >Roll No</label>
     <input class="input" type="text" name='roll_no'>
+    <label class="ff-tanger fs-1" for="roll_no">Format: <span class="ff-bunge fs-1">4CS-</span> </label>
     <button class="button" type="submit">Vote</button>
     </form>
     </div>
