558da43fe5cc0762efa146b52ea83f6f4585163f
aunglynn550 <aunglynn550@gmail.com>
2022-12-28 23:32:06 +0630

    modf-title

--- resources/views/allking/index.blade.php
+++ resources/views/allking/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <div class="container">
-    <h1 class="ff-tanger">All Kings List</h1>
+    <h1 class="ff-tanger">The Whole Kings List</h1>
 <div class="grid">
     
 
--- resources/views/allqueen/index.blade.php
+++ resources/views/allqueen/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <div class="container">
-    <h1 class="ff-tanger">All Queens List</h1>
+    <h1 class="ff-tanger">The Whole Queens List</h1>
 <div class="grid">
     
 
